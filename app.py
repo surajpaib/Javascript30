4f0fb45c20b928eec04651713ff14cd4ca8fad6f
Suraj Pai <surajballambat@gmail.com>
2016-12-10 10:08:06 +0530

    Updated for Heroku

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+app=Flask(__name__)
+app.debug=True
+
+
+
+
+@app.route('/indianmusic')
+def indianmusic():
+    render_template(index.html)
+
+
+if __name__=="__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0",port=port)
