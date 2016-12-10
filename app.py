8edb94ec785d123789e5e1bec2b18f43629a3ccb
Suraj Pai <surajballambat@gmail.com>
2016-12-10 10:12:27 +0530

    updated

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app.debug=True
 
 
 
-@app.route('/indianmusic')
+@app.route('/indianmusic',method='GET')
 def indianmusic():
-    render_template(index.html)
+    return render_template(index.html)
 
 
 if __name__=="__main__":
