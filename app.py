4e79e186f2bcf51cf3c2f7bba7659036f74ee642
Suraj Pai <surajballambat@gmail.com>
2016-12-14 19:18:34 +0530

    Updated for Heroku deployment

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 import os
 app=Flask(__name__)
 app.debug=True
 
 
 
-
-@app.route('/indianmusic',method='GET')
+@app.route('/indianmusic',methods=['GET'])
 def indianmusic():
-    return render_template(index.html)
+    url_for('static', filename='style.css')
+
+    return render_template('index.html')
 
 
 if __name__=="__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host="0.0.0.0",port=port)
+    app.run(host="127.0.0.1",port=port)
